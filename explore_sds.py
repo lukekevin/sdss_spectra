dd24947e9d4961c27a870cbdd82d1c71d9b63a5e
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-10-25 16:14:54 +0200

    Update explore_sds.py

--- explore_sds.py
+++ explore_sds.py
@@ -60,41 +60,80 @@ class fit_obj:
         print(self.spectra_filtered_list.shape)
 
     def decision_tree_sky_model(self):
-        # Use the row index (fiber index) as the feature, and the spectra as the target
         spectra_filtered_list_sky_array=np.array(self.spectra_filtered_list['sky'])
-        indices= np.arange(spectra_filtered_list_sky_array.shape[0]).reshape(-1,1)
-        #x_train, x_test=train_test_split(spectra_filtered_list_array, test_size=0.1, random_state=42)
-        tree =DecisionTreeRegressor()
-        tree.fit(indices, spectra_filtered_list_sky_array) #tree.fit(features, targets)
-        #Generate the master sky from prediction
-        master_sky=tree.predict( np.array(indices.shape[0]).reshape(-1,1))
+        
+        wavebins = spectra_filtered_list_sky_array.shape[1]
+        num_spectra = spectra_filtered_list_sky_array.shape[0]
+        master_sky_spectrum = np.zeros(wavebins)
+
+        # Running over each intensity value at a wavebin
+        for i in range(wavebins):
+            # Intensities for the i-th wavelength bin for all fibres
+            flux_at_wavebin = spectra_filtered_list_sky_array[:, i]
+            fibres=np.arange(num_spectra).reshape(-1,1)
+            regressor = DecisionTreeRegressor(max_depth=3)  
+            regressor.fit(fibres, flux_at_wavebin)  
+            pred_values = regressor.predict(fibres)
+            master_sky_spectrum[i] = pred_values.mean()
+
+        print("Generated Master Sky Spectrum:", master_sky_spectrum)
 
         #scaling the master sky to the science spectra
-        ref_master= master_sky[0][3600:3750]
-        ref_sky=self.spectra_filtered_list['sky'][:,3600:3750]
+        ref_master= master_sky_spectrum[3600:3750]
+        ref_sky=spectra_filtered_list_sky_array[:,3600:3750]
         scaling=np.mean(ref_sky/ref_master)
-        master_scaled= master_sky[0]*scaling
+        master_scaled= master_sky_spectrum[0]*scaling
 
 
-        #Subtract the scaled master from one of the fibers to get residuals
-        sub=master_scaled- self.spectra_filtered_list[1]['sky']
+        #Subtract the master from one of the fibers to get residuals
+        sub=master_scaled- spectra_filtered_list_sky_array[1]
         #Convert to flux units
         sub_flux = sub* 10**-17 * u.Unit('erg cm-2 s-1 AA-1') 
-        flux=self.spectra_filtered_list[1]['sky']* 10**-17 * u.Unit('erg cm-2 s-1 AA-1') 
-        master_sky_flux=master_sky[0]* 10**-17 * u.Unit('erg cm-2 s-1 AA-1') 
+        sky_flux=self.spectra_filtered_list['sky'][1]* 10**-17 * u.Unit('erg cm-2 s-1 AA-1') 
+        master_sky_flux=master_sky_spectrum* 10**-17 * u.Unit('erg cm-2 s-1 AA-1') 
+        
         
         fig,ax=plt.subplots(nrows=3,ncols=1,figsize=(10,7))
         ax[0].plot(10**self.spectra_filtered_list[1]['loglam'] * u.AA,
                    sub_flux, label='residual (sub from master sky)')
-        ax[0].legend()
+        ax[0].legend()def decision_tree_sky_model(self):
+        spectra_filtered_list_sky_array=np.array(self.spectra_filtered_list['sky'])
+        
+        wavebins = spectra_filtered_list_sky_array.shape[1]
+        num_spectra = spectra_filtered_list_sky_array.shape[0]
+        master_sky_spectrum = np.zeros(wavebins)
+
+        # Running over each intensity value at a wavebin
+        for i in range(wavebins):
+            # Intensities for the i-th wavelength bin for all fibres
+            flux_at_wavebin = spectra_filtered_list_sky_array[:, i]
+            fibres=np.arange(num_spectra).reshape(-1,1)
+            regressor = DecisionTreeRegressor(max_depth=3)  
+            regressor.fit(fibres, flux_at_wavebin)  
+            pred_values = regressor.predict(fibres)
+            master_sky_spectrum[i] = pred_values.mean()
+
+        print("Generated Master Sky Spectrum:", master_sky_spectrum)
+
+        #scaling the master sky to the science spectra
+        ref_master= master_sky_spectrum[3600:3750]
+        ref_sky=spectra_filtered_list_sky_array[:,3600:3750]
+        scaling=np.mean(ref_sky/ref_master)
+        master_scaled= master_sky_spectrum[0]*scaling
+
+
+        #Subtract the master from one of the fibers to get residuals
+        sub=master_scaled- spectra_filtered_list_sky_array[1]
+        #Convert to flux units
         ax[1].plot(10**self.spectra_filtered_list[1]['loglam'] * u.AA, 
-                   flux, label='sky')
+                   sky_flux, label='sky')
         ax[1].legend()
         ax[2].plot(10**self.spectra_filtered_list[1]['loglam'] * u.AA, 
                    master_sky_flux, label='master_sky from DTR')
         ax[2].legend()
         ax[0].set_title('Effects of Decison Tree for master sky from {0} fibres'.format(self.spectra_filtered_list.shape[0]))
 
+ 
  
     def fibre_correlation_plot(self, fibre_1, fibre_2):
         self.fibre_1=fibre_1
